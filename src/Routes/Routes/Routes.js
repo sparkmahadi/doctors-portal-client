5ff3933227c0a43751b33d589a0ae02a365fa6a6
sparkmahadi <mahadi.development@gmail.com>
2023-01-07 22:02:20 +0600

    added live server

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -63,7 +63,7 @@ const router = createBrowserRouter([
             {
                 path: '/dashboard/payment/:id',
                 element: <AdminRoute><Payment></Payment></AdminRoute>,
-                loader: ({params}) => fetch(`http://localhost:5000/bookings/${params.id}`)
+                loader: ({params}) => fetch(`https://doctors-portal-server-two-ivory.vercel.app/bookings/${params.id}`)
             },
         ]
     }
